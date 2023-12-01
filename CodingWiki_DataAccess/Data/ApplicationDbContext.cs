7ee743e66c64263745e11cf01d0545b6662e96c6
Malik Person <malik.person@hotmail.com>
2023-12-01 17:24:47 -0600

    *Added Model 'Book' *Added ApplicationDBContext *Auto Generated Migrations classes added due to added and applied migration *Reference to CodingWiki_Model added to CodingWiki_DataAccess to allow for access of data to be sent to and from the database

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Sensitive information
+            optionsBuilder.UseSqlServer("Server=MPSPC2022\\MSSQLSERVER02;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ CodingWiki_Model/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_Model.Models
+{
+    public class Book
+    {
+        [Key]
+        public int IdBook { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public double Price { get; set; }
+    }
+}
