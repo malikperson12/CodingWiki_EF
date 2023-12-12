fe3a50d88c8170675a5f54e5229bdb22db37e900
Malik Person <malik.person@hotmail.com>
2023-12-12 12:57:44 -0600

    1. one to one relation Book class and Book Detail class migration

---
+++ CodingWiki_DataAccess/Migrations/20231212184236_AddOneToOneRelationBookAndBookDetail.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWikiDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOneToOneRelationBookAndBookDetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdBook",
+                table: "Books",
+                newName: "Book_Id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Book_Id",
+                table: "BookDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookDetails_Book_Id",
+                table: "BookDetails",
+                column: "Book_Id",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookDetails_Books_Book_Id",
+                table: "BookDetails",
+                column: "Book_Id",
+                principalTable: "Books",
+                principalColumn: "Book_Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookDetails_Books_Book_Id",
+                table: "BookDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookDetails_Book_Id",
+                table: "BookDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Book_Id",
+                table: "BookDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "Book_Id",
+                table: "Books",
+                newName: "IdBook");
+        }
+    }
+}
